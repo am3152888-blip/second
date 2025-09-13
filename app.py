a97098323fbd6b3b6c16d1e53c8b513d123369b5
am3152888-blip <am3152888@gmail.com>
2025-09-13 17:29:40 +0300

    ddd

--- app.py
+++ app.py
@@ -556,11 +556,7 @@
 # print(peoples["Ahmed"]['Css'])
 # print(peoples["Sayed"]['Css'])
 
-# print(peoples["Osama"]['Css'])
-# print(peoples["Ahmed"]['Css'])
-# print(peoples["Sayed"]['Css'])  edit 
-# login and check
-# login two
+
 
 # for name in peoples:
 
